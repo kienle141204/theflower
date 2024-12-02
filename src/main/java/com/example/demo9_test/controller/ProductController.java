dcf1d02e52047d8ec9a8034e0631f40f425ce8e0
KienLee <Kien.LT225023@sis.hust.edu.vn>
2024-12-02 17:33:48 +0700

    product

--- src/main/java/com/example/demo9_test/controller/ProductController.java
+++ src/main/java/com/example/demo9_test/controller/ProductController.java
@@ -3,14 +3,17 @@ package com.example.demo9_test.controller;
 import com.example.demo9_test.repository.ProductRepository;
 import com.example.demo9_test.entity.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ProductController {
@@ -22,4 +25,12 @@ public class ProductController {
     public List<Product> getAllProducts() {
         return productRepository.findAll(); // Lấy tất cả sản phẩm từ cơ sở dữ liệu
     }
+
+    @GetMapping("/api/products/{productId}")
+    public ResponseEntity<Product> getProductById(@PathVariable("productId") Long productId) {
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new RuntimeException("loi"));
+        return ResponseEntity.ok(product);
+    }
+
 }
